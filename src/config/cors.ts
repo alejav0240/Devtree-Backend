4b62cc21574fca18bb427a152afa57ca6017b957
alejav0240 <cra2028252@est.univalle.edu>
2025-02-03 21:11:45 -0400

    Añadir registro de la lista blanca de CORS y mejorar la gestión de errores

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,16 +1,17 @@
 import { CorsOptions } from "cors";
+import colors from "colors";
 
 export const corsConfig: CorsOptions = {
-    origin: function (origin, callback) {
-        const whiteList = process.env.CORS_FRONEND_LIST?.split(",");
-        if (process.argv.includes("--api")) {
-            whiteList.push(undefined)
-        }
-
-        if (whiteList.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS "+ origin));
-        }
+  origin: function (origin, callback) {
+    const whiteList = process.env.CORS_FRONEND_LIST?.split(",");
+    if (process.argv.includes("--api")) {
+      whiteList.push(undefined);
     }
+    console.log(colors.bgCyan.white.bold("CORS: "), whiteList);
+    if (whiteList.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS " + origin));
+    }
+  },
 };
